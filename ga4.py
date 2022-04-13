4062e57d0c400127f36cebac0ad7ddc23cac5b58
Eduardo Mendez <eduardo@pukara.es>
2022-04-13 12:43:59 +0200

    Add queries for some dimensions for the metric active users

--- ga4.py
+++ ga4.py
@@ -1,4 +1,5 @@
 import os
+import time
 from os.path import join, dirname
 from dotenv import load_dotenv
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
@@ -17,16 +18,20 @@ client = BetaAnalyticsDataClient()
 
 property_id = os.getenv('PROPERTY_ID')
 
-request = RunReportRequest(
+
+dimesions = ['userAgeBracket', 'language', 'brandingInterest', 'userGender'] 
+
+# Example of one request for each dimension with activeUsers metric
+for dimesion in dimesions:
+    request = RunReportRequest(
         property=f"properties/{property_id}",
-        dimensions=[Dimension(name="city")],
+        dimensions=[Dimension(name=dimesion)],
         metrics=[Metric(name="activeUsers")],
         date_ranges=[DateRange(start_date="2022-04-11", end_date="today")],
-)
+    )
+    time.sleep(5)
+    response = client.run_report(request)
 
-response = client.run_report(request)
-
-print("Result:")
-for row in response.rows:
+    print(f'Results of active users by {dimesion} dimension')
+    for row in response.rows:
         print(row.dimension_values[0].value, row.metric_values[0].value)
-
